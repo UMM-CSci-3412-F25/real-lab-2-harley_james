9fb2c3590abe410e04d284c5ee0245c584e55416
James Swearingen <swear051@morris.umn.edu>
2025-09-16 11:39:44 -0500

    disemvowelment

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,42 @@
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
 #include "disemvowel.h"
 
-char *disemvowel(char *str) {
-  return (char*) "";
+bool is_vowel(char vowel) {
+  return (
+    vowel == 'a' || vowel == 'A' ||
+    vowel == 'e' || vowel == 'E' ||
+    vowel == 'i' || vowel == 'I' ||
+    vowel == 'o' || vowel == 'O' ||
+    vowel == 'u' || vowel == 'U'
+  );
 }
+
+char *disemvowel(char *str) {
+  int non_vowels = 0;
+  int length = strlen(str);
+  for (int i=0; i<length; i++) {
+    if (!is_vowel(str[i])) {
+      non_vowels++;
+    }
+  }
+  char *result = (char*) calloc(non_vowels+1, sizeof(char));
+  int j = 0;
+  for (int i=0; i<length; i++) {
+    if (!is_vowel(str[i])) {
+      if (result != NULL) {
+        result[j] = str[i];
+      }
+      j++;
+    }
+  }
+  if (result != NULL) {
+    result[non_vowels] = '\0';
+  }
+  
+  return result;
+}
+
+
